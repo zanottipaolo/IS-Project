d0aab4fb89c40394e8dd882da806d602529040ed
Paolo Zanotti <zanotti.paolo2001@gmail.com>
2022-12-15 15:47:43 +0100

    💄 Add MUI

--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import Dashboard from '../components/Dashboard'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Head>
+        <link rel="icon" href="/images/website_icon.ico" />
+      </Head>
+
+      <Dashboard />
+
+      <Component {...pageProps} />
+    </>
+  )
 }
